8fdedf09b679c9189432c0e1db86ebd94d4007de
salma hamdi <saalmahm51@gmail.com>
2025-09-18 11:28:22 +0100

    Crée le model Versement

--- src/model/Versement.java
+++ src/model/Versement.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Versement {
+public class Versement extends Operation {
+    private String source;
+
+    public Versement(double montant, String source) {
+        super(montant);
+        this.source = source;
+    }
+
+    public String getSource() { return source; }
+
+    @Override
+    public String getType() { return "VERSEMENT"; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " source=" + source;
+    }
 }
