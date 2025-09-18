d7a968f13efc52759ee946dc618583eb7bb92a2d
salma hamdi <saalmahm51@gmail.com>
2025-09-18 11:25:59 +0100

    Crée le model de class compte courant

--- src/model/CompteCourant.java
+++ src/model/CompteCourant.java
@@ -1,4 +1,41 @@
 package model;
 
-public class CompteCourant {
+public class CompteCourant extends Compte {
+    private double decouvert; // allowed overdraft (positive number)
+
+    public CompteCourant(String code, double initialSolde, double decouvert) {
+        super(code, initialSolde);
+        if (decouvert < 0) throw new IllegalArgumentException("Découvert must be >= 0");
+        this.decouvert = decouvert;
+    }
+
+    public double getDecouvert() {
+        return decouvert;
+    }
+    // float x = 3.14f
+    // double y = 3.14
+    //  x = y;
+    //  y = x; // error
+
+    @Override
+    public void retirer(double montant, String destination) {
+        if (montant <= 0) throw new IllegalArgumentException("Montant doit être positif");
+        double newSolde = solde - montant;
+        if (newSolde < -decouvert) {
+            throw new IllegalArgumentException("Retrait refusé : dépasse le découvert autorisé.");
+        }
+        solde = newSolde;
+        Retrait r = new Retrait(montant, destination);
+        addOperation(r);
+    }
+
+    @Override
+    public double calculerInteret() {
+        return 0.0;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Compte Courant - code: " + code + " | solde: " + solde + " | découvert: " + decouvert);
+    }
 }
